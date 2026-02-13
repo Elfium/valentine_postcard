8c9ec9a744acdaa50b32c5cba6621f23a7b6bbec
Elfium <3434kayn@gmail.com>
2026-02-14 00:35:23 +0300

    fix_3

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771017974|2821218048';
+const CACHE_VERSION = '1771018497|3344757997';
 /** @type {string} */
 const CACHE_PREFIX = '14postcard-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
